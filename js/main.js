af52d90f7df086e58480641daae02086b8f84d9a
Jeferson Domingos <jefersondomingos77@gmail.com>
2022-11-08 23:52:36 -0300

    Persistindo os dados

--- js/main.js
+++ js/main.js
@@ -1,41 +1,46 @@
 const formulario = document.getElementById("novoItem");
 const lista = document.getElementById("lista");
-const itens = [];
+const itens = JSON.parse(localStorage.getItem("itens")) || [];
+//persistência do item criado
+itens.forEach((elemento) => {
+    criaElemento(elemento);
+});
 
 formulario.addEventListener("submit", (evento) => {
     evento.preventDefault();
-    const inputNome = evento.target.elements['nome'];
-    const inputQuantidade = evento.target.elements['quantidade'];
-    
-    if(inputQuantidade.value > 0 && inputNome.value !== ""){
-        criaElemento (inputNome.value, inputQuantidade.value);
-        inputNome.value = ""; 
-        inputQuantidade.value = "";
-    }
 
+    const nome = evento.target.elements['nome'];
+    const quantidade = evento.target.elements['quantidade'];
+
+    const itemAtual = {
+        "nome" : nome.value,
+        "quantidade" : quantidade.value
+    };
+
+    criaElemento (itemAtual);
+
+    itens.push(itemAtual);
+                         //chave , valor
+    localStorage.setItem("itens" , JSON.stringify(itens));
+    
+    nome.value = "";
+    quantidade.value = "";
+    
 });
 
-function criaElemento (nome, quantidade){
+function criaElemento (itemAtual){
     //<li class="item"><strong>1</strong></li>
     const novoItem = document.createElement('li');
     novoItem.classList.add("item");
 
     const numeroItem = document.createElement('strong');
-    numeroItem.innerHTML = quantidade;
+    numeroItem.innerHTML = itemAtual.quantidade;
 
     novoItem.appendChild(numeroItem);
-    novoItem.innerHTML += nome;
+    novoItem.innerHTML += itemAtual.nome;
 
     lista.appendChild(novoItem);
+};
 
-    const itemAtual = {
-        "nome" : nome,
-        "quantidade" : quantidade
-    };
-
-    itens.push(itemAtual);
-                        //chave , valor
-    localStorage.setItem("item" , JSON.stringify(itens));
-}
 
 
