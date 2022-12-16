f41911d31853e9aba8937016fe4771dccc4acc51
Jeferson Domingos <105239532+JefersonDomingos@users.noreply.github.com>
2022-12-15 21:36:45 -0300

    projeto finalizado

--- js/main.js
+++ js/main.js
@@ -22,12 +22,16 @@ form.addEventListener("submit", (evento) => {
     };
 
     if(existe){
+        //se o nome for igual, o id também será
         itemAtual.id = existe.id;
-        console.log("Já existe e o id é: " + existe.id);
         atulizaLista(itemAtual);
+        
+        //se o id for igual, então o que já está no local storage vai receber o valor que estou criando
+        itens[itens.findIndex(elemento => elemento.id === existe.id)] = itemAtual;
     
-    }else{
-        itemAtual.id = itens.length;
+    }else{                  
+        //se tiver algum elemento criado no itens[], o 'id' do itemAtual vai ser o 'id' do ultimo elemento + 1, senão o id será 0;                                  
+        itemAtual.id = itens[itens.length -1] ? (itens[itens.length -1]).id + 1 : 0 ;
         criaElemento(itemAtual);
         itens.push(itemAtual);
     }
@@ -49,10 +53,33 @@ function criaElemento(item) {
     novoItem.appendChild(numeroItem);
     
     novoItem.innerHTML += item.nome;
+    novoItem.appendChild(criaBotaoDeletar(item.id));
     lista.appendChild(novoItem);
+
 };
 
 function atulizaLista(item){
-    console.log("atualizando")
+    console.log("atualizando");
     document.querySelector("[data-id='"+item.id+"']").innerHTML = item.quantidade;
-}
+};
+
+function criaBotaoDeletar (id){
+    const botaoDeletar = document.createElement("button");
+    botaoDeletar.innerText = "x";
+
+    botaoDeletar.addEventListener('click', function () {
+        deletarItem(this.parentNode,id);
+    });
+    
+    return botaoDeletar;
+};
+
+function deletarItem (tag,id){
+    tag.remove();
+    
+    //deletar o indice do array itens, que for igual ao id do item clicado.
+    itens.splice(itens.findIndex(elemento => elemento.id === id),1);
+    console.log(itens);
+
+    localStorage.setItem("itens", JSON.stringify(itens));
+};
